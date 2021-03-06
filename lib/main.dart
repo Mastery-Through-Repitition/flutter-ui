f209bfc68e87b76a31c692813420454a5eba57a8
Johan Jarvi <johan.jarvi@gmail.com>
2021-03-07 08:34:20 +1000

    STEP-4: 2/3 - Create build suggestions private widget

--- lib/main.dart
+++ lib/main.dart
@@ -40,4 +40,26 @@ class _RandomWordsState extends State<RandomWords> {
     final WordPair wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
   }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+    );
+  }
 }
