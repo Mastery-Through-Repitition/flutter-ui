ee69a292010d3f04f16d5c058163d036753f83b0
Johan Jarvi <johan.jarvi@gmail.com>
2021-03-07 09:33:54 +1000

    PART-2: 6 - Add a new 'page' for showing favourites

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Startup Name Generator'),
+          actions: [IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))],
         ),
         body: _buildSuggestions());
   }
@@ -64,5 +65,23 @@ class _RandomWordsState extends State<RandomWords> {
         });
       },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map((WordPair pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: _biggerFont),
+        );
+      });
+
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+          appBar: AppBar(title: Text('Saved Suggestions')),
+          body: ListView(children: divided));
+    }));
   }
 }
